3e7c417f61cbc50eccd4c869b7a709d8e9bd7977
RodrigoGondim <111583608+RodrigoGondim@users.noreply.github.com>
2023-02-07 22:04:11 -0300

    Add files via upload
    '''Ex002 - Crie um programa que leia um nome e mostre uma mensagem de saudação '''

---
+++ ex002 - input().py
@@ -0,0 +1,4 @@
+'''Ex002 - Crie um programa que leia um nome e mostre uma mensagem de saudação '''
+
+your_name = str(input('Qual é o seu nome: '))
+print(f'Olá {your_name}, é um prazer em te conhecer!')
