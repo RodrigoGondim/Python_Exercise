1c5ef4272ef18c9399f61dd0b0293f1b5e5fa529
RodrigoGondim <111583608+RodrigoGondim@users.noreply.github.com>
2023-02-10 19:11:30 -0300

    Add files via upload

---
+++ ex005 - Antecessor e Sucessor.py
@@ -0,0 +1,4 @@
+'''Ex005 - Faça um programa que leia um inteiro e mostre na tela seu sucessor e antecessor'''
+
+int_number = int(input('Digite um número inteiro: '))
+print(f'Você digitou o número {int_number} e seu antecessor é {int_number-1} e seu sucessor é {int_number+1}')
---
+++ ex006 - Dobro, triplo e raiz quadrada.py
@@ -0,0 +1,6 @@
+'''Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.'''
+int_number = int((input('Escolha um número: ')))
+print(f'''O número que você escolheu foi {int_number} 
+Seu dobro é {int_number * 2}
+Seu tripo é {int_number * 3}
+E sua raiz² é {int_number ** 0.5}''')
