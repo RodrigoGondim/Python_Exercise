4d606a4a1d205ebf469b17b6ecf05f82524bfa3c
RodrigoGondim <111583608+RodrigoGondim@users.noreply.github.com>
2023-02-13 16:50:43 -0300

    Add files via upload

---
+++ ex009 - Tabuada.py
@@ -0,0 +1,12 @@
+'''Exercício Python 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.'''
+multiplication_table = int(input('Digite um número para ver a sua tabuada: '))
+print(f'{multiplication_table} X {1:2} = {multiplication_table * 1}')
+print(f'{multiplication_table} X {2:2} = {multiplication_table * 2}')
+print(f'{multiplication_table} X {3:2} = {multiplication_table * 3}')
+print(f'{multiplication_table} X {4:2} = {multiplication_table * 4}')
+print(f'{multiplication_table} X {5:2} = {multiplication_table * 5}')
+print(f'{multiplication_table} X {6:2} = {multiplication_table * 6}')
+print(f'{multiplication_table} X {7:2} = {multiplication_table * 7}')
+print(f'{multiplication_table} X {8:2} = {multiplication_table * 8}')
+print(f'{multiplication_table} X {9:2} = {multiplication_table * 9}')
+print(f'{multiplication_table} X {10:2} = {multiplication_table * 10}')
